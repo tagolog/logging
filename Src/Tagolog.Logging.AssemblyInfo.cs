53b6ab7600b869d0fb3d4474afafd1436d73b17b
w <w@tagolog.com>
2017-03-15 19:48:36 +0300

    Upgraded to 0.6 to fix 0.5/04 version mess in previous version.

--- Src/Tagolog.Logging.AssemblyInfo.cs
+++ Src/Tagolog.Logging.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright( "Copyright © 2013-2016 Tagolog Team" )]
 [assembly: AssemblyTrademark( "Apache License, Version 2.0" )]
 
-[assembly: AssemblyVersion( "0.4.0.0" )]
+[assembly: AssemblyVersion( "0.6.0.0" )]
 
-[assembly: AssemblyFileVersion( "0.4.0.0" )]
-[assembly: AssemblyInformationalVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion( "0.6.0.0" )]
+[assembly: AssemblyInformationalVersion("0.6.0.0")]
